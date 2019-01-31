d8091e1016551c7073635dc578f299648e7b98a5
MrJouts <30276816+MrJouts@users.noreply.github.com>
2019-01-30 22:40:58 -0300

    cnx

--- api/conexion.php
+++ api/conexion.php
@@ -1,4 +1,4 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
-$db = new PDO('mysql:host=localhost:3306;dbname=dw6_radiko;charset=utf8', 'root', 'root');
+$db = new PDO('mysql:host=localhost:3306;dbname=dw6_radiko;charset=utf8', 'root', '');
